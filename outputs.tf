951cb27b3d78867770ce5777d07070a9bf5e8751
itgenius-devops <info@itgenius.io>
2025-04-20 10:20:52 -0400

    updated deployment for ansible servers

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 output "instance_public_ips" {
   value = [for instance in aws_instance.servers : instance.public_ip]
 }
@@ -6,12 +5,3 @@ output "instance_public_ips" {
 output "instance_ids" {
   value = [for instance in aws_instance.servers : instance.id]
 }
-=======
-output "instance_public_ips" {
-  value = [for instance in aws_instance.servers : instance.public_ip]
-}
-
-output "instance_ids" {
-  value = [for instance in aws_instance.servers : instance.id]
-}
->>>>>>> cf96b1381fffa43047bc10e1c82716d87a226ea6
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 key_name = "ansible_key"
-=======
-key_name = "ansible_key"
->>>>>>> cf96b1381fffa43047bc10e1c82716d87a226ea6
